d7e802cb4f9195698e082de3a83b524d42069ab9
Yuri Vidal <yurividal123@hotmail.com>
2023-09-26 18:48:52 -0300

    feito build, criado pasta no dist para js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*scss')
@@ -7,7 +14,10 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+exports.default = gulp.parallel(scripts);
+exports.default = (styles, scripts);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
