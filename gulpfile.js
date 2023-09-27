b4733d56da3d71a2e0f49b1e29d94066f700bff8
Yuri Vidal <yurividal123@hotmail.com>
2023-09-27 14:28:09 -0300

    feito

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,14 @@ function scripts() {
     .pipe(gulp.dest('./dist/js'))
 }
 
+
 function styles() {
-    return gulp.src('./src/styles/*scss')
-        .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./dist/css'));
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({outputStyles: 'compressed' }))
+    .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = gulp.parallel(scripts);
-exports.default = (styles, scripts);
+exports.default = gulp.parallel(styles, scripts);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*scss', gulp.parallel(styles))
